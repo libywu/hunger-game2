79b65bfce3aefc1cd09600fd71f85f821ce315d4
libywu <147145616+libywu@users.noreply.github.com>
2024-05-23 11:40:14 +0800

    Hunger game all assets
    项目资源+成员分工+开发说明+游戏演示

---
+++ PythonManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.IO;
+using UnityEditor.Scripting.Python;
+using UnityEngine;
+
+
+public class PythonManager : MonoBehaviour
+{
+
+    [System.Obsolete]
+
+    void Update()
+    {
+        string path = Application.dataPath + "/Python/pythonTest.py";
+        PythonRunner.RunFile(path);
+    }
+
+
+
+}
